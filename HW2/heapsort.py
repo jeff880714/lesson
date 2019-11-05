ac3cbf600426baab0fdb375dea441950eb1dba83
jeff880714 <55685271+jeff880714@users.noreply.github.com>
2019-11-05 22:32:56 +0800

    Update heapsort.py

--- HW2/heapsort.py
+++ HW2/heapsort.py
@@ -18,3 +18,4 @@ class Solution():
             results.append(nums.pop(0))#每次取最大值出來
         results.reverse()#因為是max heap取出來的list要反轉
         return results
+#參考資料:https://github.com/wellslu/DSA/blob/master/HW2/heap_sort_06170107.py
