9b4389e719820e41816b924d5f9ab0ffa6a7e180
jeff880714 <55685271+jeff880714@users.noreply.github.com>
2019-11-22 16:21:53 +0800

    Update binary_search_tree_06170118.py

--- HW3/binary_search_tree_06170118.py
+++ HW3/binary_search_tree_06170118.py
@@ -3,12 +3,29 @@ class TreeNode(object):
         self.val = x
         self.left = None
         self.right = None
-        
 class Solution(object):
     def insert(self, root, val):
-        
+        if val <= root.val and root.left is None:
+            root.left = TreeNode(val)
+            return root.left
+        if val > root.val and root.right is None:
+            root.right = TreeNode(val)
+            return root.right
+        if val <= root.val and root.left:
+            root = root.left
+        if val> root.val and root.right:
+            root = root.right
+            
+    def search(self, root, val):
+        if root is None:
+            return root
+        if root.val == val:
+            return root
+        if root.val < val:
+            return self.search(root.right,val)
+        if root.val > val:
+            return self.search(root.left,val)
     def delete(self, root, target):
-
-    def search(self, root, target):
-
+        pass
     def modify(self, root, target, new_val):
+        pass
